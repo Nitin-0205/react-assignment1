7bdfaf2f71fbc7a7fd35f70dd2bc8ea85c1b45c2
Nitin Gupta <nitingupta1906@gmail.com>
2021-08-18 01:05:34 +0530

    AssignMent Day 1

---
+++ src/head.js
@@ -0,0 +1,20 @@
+import "./index.css"
+
+
+const Head = ()=>{
+    return (
+        <header>
+            <h2>Nitin Blog Channel</h2>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Servicer</li>
+                <li>Contact</li>
+
+            </ul>
+
+        </header>
+    );
+}
+
+export default Head;
---
+++ src/main.js
@@ -0,0 +1,15 @@
+import "./index.css"
+
+
+const Main = ()=>{
+    return (
+        <main>
+            <p>I am Main Container  
+                How are You 
+                What Would you like to take
+            </p>
+        </main>
+    );
+}
+
+export default Main;
